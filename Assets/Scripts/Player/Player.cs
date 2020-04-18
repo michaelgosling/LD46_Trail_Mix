1d18304d3304bab15bfd1880b71bbaa4c893e8eb
Michael Gosling <mgosling94@gmail.com>
2020-04-18 19:34:35 -0300

    rename Player Controller to Player, add some default properties.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public static readonly int DEFAULT_HP = 100;
+
+    public bool CarryingIt { get; private set; } = true;
+    public int HP { get; private set; } = DEFAULT_HP;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+    }
+
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 v2 = new Vector2(Input.GetAxis("Horizontal"), 0);
+        moveVelocity = v2.normalized * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
